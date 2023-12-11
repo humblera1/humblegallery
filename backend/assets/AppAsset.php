5667321ac0d5de8e226f1cc65402ea357f28e419
maxim koshkin <humblerat@mail.ru>
2023-12-11 21:32:31 +0700

    add custom style files

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,6 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/bootstrap/bootstrap.css',
+        'css/admin.css',
         'css/site.css',
     ];
     public $js = [

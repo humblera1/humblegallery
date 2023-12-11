9e59edbea5b170afd917c566bd4ae8cf12b8fbff
maxim koshkin <humblerat@mail.ru>
2023-12-11 22:00:51 +0700

    login template, page, styles

--- backend/config/main.php
+++ backend/config/main.php
@@ -7,7 +7,7 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-backend',
+    'id' => 'HUMBLEGALLERY',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],

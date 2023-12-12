1e7ff79c33d93366ff372770a9aa7d589c65f53e
maxim koshkin <humblerat@mail.ru>
2023-12-12 21:50:04 +0700

    admin panel template

--- backend/views/layouts/includes/_header.php
+++ backend/views/layouts/includes/_header.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
             <li class="nav-item">
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
-            <li class="nav-item d-none d-sm-inline-block">
+            <li class="nav-item">
                 <a href="<?= Url::home() ?>" class="nav-link">Главная</a>
             </li>
         </ul>

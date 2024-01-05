4e63e080aa8ffaa282b6fe2e1418a4a33a0a6b75
maxim koshkin <humblerat@mail.ru>
2024-01-05 18:57:06 +0700

    create frontend page

--- common/modules/painting/views/frontend/default/index.php
+++ common/modules/painting/views/frontend/default/index.php
@@ -1,7 +1,56 @@
 <?php
 
-use yii\helpers\Html;
+use common\modules\painting\models\data\Painting;
+use yii\data\Pagination;
+use yii\web\View;
+use yii\widgets\LinkPager;
 
-$model = \common\modules\painting\models\data\Painting::findOne(2);
+/**
+ * @var $this View
+ * @var Painting[] $models
+ * @var Pagination $pages
+ */
 
-echo Html::img($model->service->getImagePath()); ?>
+?>
+
+<div class="page">
+    <div class="page__content">
+        <header class="header">
+            <nav class="header__navigation">
+                <!-- Header navigation content goes here -->
+            </nav>
+        </header>
+
+        <div class="page-container">
+            <aside class="sidebar">
+                <!-- Sidebar content goes here -->
+            </aside>
+
+            <main class="content">
+                <?php foreach($models as $model): ?>
+                    <div class="paint">
+                        <div class="paint__paint">
+                            <?= $model->title ?>
+                            <!-- picture goes here -->
+                        </div>
+                        <div class="paint__description">
+                            <!-- description goes here -->
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+
+            </main>
+
+            <?= LinkPager::widget([
+                'pagination' => $pages,
+            ]); ?>
+        </div>
+
+
+
+        <!--    <footer class="footer">-->
+        <!--        Footer content goes here -->
+        <!--    </footer>-->
+    </div>
+</div>
+

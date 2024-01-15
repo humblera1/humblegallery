050ccafdb99fe5999d2e2183043e727d5a7edf3d
maxim koshkin <humblerat@mail.ru>
2024-01-15 22:19:36 +0700

    header

--- common/modules/painting/views/frontend/default/index.php
+++ common/modules/painting/views/frontend/default/index.php
@@ -1,13 +1,9 @@
 <?php
 
 use common\modules\painting\models\search\PaintingSearch;
-use common\modules\subject\models\data\Subject;
 use frontend\assets\MasonryAsset;
 use yii\data\ActiveDataProvider;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
 use yii\web\View;
-use yii\widgets\ActiveForm;
 use yii\widgets\Pjax;
 
 /**
@@ -17,39 +13,16 @@ use yii\widgets\Pjax;
  */
 
 MasonryAsset::register($this);
+
 ?>
 
-<div class="page">
-    <div class="page__content">
-        <header class="header">
-            <nav class="header__navigation">
-                <div class="nav-block">
-                    <div class="nav-item--logo"></div>
-                    <div class="nav-item">Художники</div>
-                    <div class="nav-item">Картины</div>
-                    <div class="nav-item">Статьи</div>
-                </div>
-                <div class="nav-block">
-                    <div class="nav-item">Профиль</div>
-                </div>
-            </nav>
-        </header>
+<?= $this->render('includes/_sidebar', ['model' => $model]) ?>
 
-        <div class="page-container">
-            <?= $this->render('includes/_sidebar', ['model' => $model]) ?>
-
-            <?php Pjax::begin() ?>
-                <div class="painting-catalog">
-                    <main class="content">
-                    <?= $this->render('includes/_content', ['provider' => $dataProvider]) ?>
-                    </main>
-                </div>
-            <?php Pjax::end() ?>
-        </div>
-    </div>
+<?php Pjax::begin() ?>
+<div class="painting-catalog">
+    <main class="content">
+        <?= $this->render('includes/_content', ['provider' => $dataProvider]) ?>
+    </main>
 </div>
-
-<?php
-
-?>
+<?php Pjax::end() ?>
 
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -24,60 +24,35 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
+<div class="page">
+    <div class="page__content">
+        <header class="header">
+            <nav class="header__navigation">
+                <div class="nav-block">
+                    <div class="nav-item--logo"></div>
+                    <div class="nav-item">Художники</div>
+                    <div class="nav-item">Картины</div>
+                    <div class="nav-item">Статьи</div>
+                </div>
+                <div class="nav-block">
+                    <?php if (Yii::$app->user->isGuest): ?>
+                        <?= Html::a('Вход', ['/site/login'], ['class' => 'nav-item']); ?>
+                    <?php else: ?>
+                        <?= Html::a('Профиль', [''], ['class' => 'nav-item']); ?>
+                    <?php endif; ?>
+                </div>
+            </nav>
+        </header>
+        <div class="page-container">
+            <?= $content ?>
 
-<!--<header>-->
-<!--    --><?php
-//    NavBar::begin([
-//        'brandLabel' => Yii::$app->name,
-//        'brandUrl' => Yii::$app->homeUrl,
-//        'options' => [
-//            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-//        ],
-//    ]);
-//    $menuItems = [
-//        ['label' => 'Home', 'url' => ['/site/index']],
-//        ['label' => 'About', 'url' => ['/site/about']],
-//        ['label' => 'Contact', 'url' => ['/site/contact']],
-//        ['label' => 'Картины', 'url' => ['/painting/default/index']],
-//    ];
-//    if (Yii::$app->user->isGuest) {
-//        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-//    }
-//
-//    echo Nav::widget([
-//        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-//        'items' => $menuItems,
-//    ]);
-//    if (Yii::$app->user->isGuest) {
-//        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-//    } else {
-//        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-//            . Html::submitButton(
-//                'Logout (' . Yii::$app->user->identity->username . ')',
-//                ['class' => 'btn btn-link logout text-decoration-none']
-//            )
-//            . Html::endForm();
-//    }
-//    NavBar::end();
-//    ?>
-<!--</header>-->
-
-<!--<main role="main" class="flex-shrink-0">-->
-<!--    <div class="container">-->
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
-        <?= $content ?>
-<!--    </div>-->
-<!--</main>-->
-
-<!--<footer class="footer mt-auto py-3 text-muted">-->
-<!--    <div class="container">-->
-<!--        <p class="float-start">&copy; --><?php //= Html::encode(Yii::$app->name) ?><!-- --><?php //= date('Y') ?><!--</p>-->
-<!--        <p class="float-end">--><?php //= Yii::powered() ?><!--</p>-->
-<!--    </div>-->
-<!--</footer>-->
+        </div>
+    </div>
+</div>
 
 <?php $this->endBody() ?>
 </body>

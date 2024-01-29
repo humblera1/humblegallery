d98c687f0afb2d1e56fe804dd679a5f38a138795
maxim koshkin <humblerat@mail.ru>
2024-01-29 22:52:51 +0700

    hello bootstrap

--- common/modules/painting/views/admin/default/view.php
+++ common/modules/painting/views/admin/default/view.php
@@ -16,14 +16,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="painting-view">
-
-    <div class="d-flex justify-content-between align-items-center py-4 px-5">
-        <h1><?= Html::encode($this->title) ?></h1>
-
-        <p>
-            <?= Html::a(Yii::t('app', 'Редактировать'), ['update', 'id' => $model->id], ['class' => 'btn btn-orange mx-3']) ?>
+    <div class="d-flex row py-4 col-txl-8 col-lg-12">
+        <h2 class="col-lg-6 col-md-12 mb-4"><?= Html::encode($model->service->getNameToDisplay()) ?></h2>
+        <p class="col-lg-6 col-md-12 d-flex justify-content-lg-end align-items-center">
+            <?= Html::a(Yii::t('app', 'Редактировать'), ['update', 'id' => $model->id], ['class' => 'btn btn-orange']) ?>
             <?= Html::a(Yii::t('app', 'Удалить'), ['delete', 'id' => $model->id], [
-                'class' => 'btn btn-danger',
+                'class' => 'btn btn-danger mx-3',
                 'data' => [
                     'confirm' => 'Are you sure you want to delete this item?',
                     'method' => 'post',
@@ -31,49 +29,74 @@ $this->params['breadcrumbs'][] = $this->title;
             ]) ?>
         </p>
     </div>
+    <div class="row gx-4">
+        <div class="col-txl-4 col-xl-6 col-lg-10">
+            <?= Html::img($model->service->getImage(), ['class' => 'img-fluid rounded-5 shadow']); ?>
+        </div>
+        <div class="col-txl-4 col-xl-6 col-lg-10">
 
-    <div class="col-md-6">
-        <?= DetailView::widget([
-            'model' => $model,
-            'options' => ['class' => 'table table-bordered table-hover detail-view'],
-            'template' => '<tr><td><strong>{captionOptions}{label}:</strong></td><td>{contentOptions}{value}</td></tr>',
-            'attributes' => [
-                'id',
-                'title',
-                [
-                    'attribute' => 'artist_id',
-                    'value' => $model->artist->name,
-                ],
-                'start_date',
-                'end_date',
-                [
-                    'attribute' => 'movements',
-                    'label' => Yii::t('app', 'Направления'),
-                    'format' => 'raw',
-                    'value' => $model->service->getMovementsList(),
-                ],
-                [
-                    'attribute' => 'subjects',
-                    'label' => Yii::t('app', 'Жанры'),
-                    'format' => 'raw',
-                    'value' => $model->service->getSubjectsList(),
-                ],
-                [
-                    'attribute' => 'technique_id',
-                    'value' => $model->technique->name,
-                ],
-                'rating',
-                [
-                    'attribute' => 'created_at',
-                    'value' => Yii::$app->formatter->asDatetime($model->created_at),
-                ],
-                [
-                    'attribute' => 'updated_at',
-                    'value' => Yii::$app->formatter->asDatetime($model->updated_at),
-                ],
-                'is_deleted:boolean',
-            ],
-        ]) ?>
+            <div class="py-3">
+                <div class="row col-12">
+                    <div class="col-4">
+                        <div class="fs-5 fw-bold">Художник:</div>
+                    </div>
+                    <div class="col-8">
+                        <div class="fs-5"><?= $model->artist->name; ?></div>
+                    </div>
+                </div>
+                <div class="row col-12">
+                    <div class="col-4">
+                        <div class="fs-5 fw-bold">Техника:</div>
+                    </div>
+                    <div class="col-8">
+                        <div class="fs-5"><?= $model->technique->name; ?></div>
+                    </div>
+                </div>
+                <div class="row col-12">
+                    <div class="col-4">
+                        <div class="fs-5 fw-bold"><?= Yii::t('app', 'Направления:') ?></div>
+                    </div>
+                    <div class="col-8">
+                        <div class="fs-5"><?= $model->service->getMovementsList(); ?></div>
+                    </div>
+                </div>
+                <div class="row col-12">
+                    <div class="col-4">
+                        <div class="fs-5 fw-bold"><?= Yii::t('app', 'Жанры:') ?></div>
+                    </div>
+                    <div class="col-8">
+                        <div class="fs-5"><?= $model->service->getSubjectsList(); ?></div>
+                    </div>
+                </div>
+            </div>
+            <div class="py-3">
+                <div class="row col-12">
+                    <div class="col-4">
+                        <div class="fs-5 fw-bold">Дата добавления:</div>
+                    </div>
+                    <div class="col-8">
+                        <div class="fs-5"><?= Yii::$app->formatter->asDatetime($model->created_at); ?></div>
+                    </div>
+                </div>
+                <div class="row col-12">
+                    <div class="col-4">
+                        <div class="fs-5 fw-bold">Дата создания:</div>
+                    </div>
+                    <div class="col-8">
+                        <div class="fs-5"><?= Yii::$app->formatter->asDatetime($model->updated_at); ?></div>
+                    </div>
+                </div>
+            </div>
+            <div class="py-3">
+                <div class="row col-12">
+                    <div class="col-4">
+                        <div class="fs-5 fw-bold">В архиве:</div>
+                    </div>
+                    <div class="col-8">
+                        <div class="fs-5"><?= $model->is_deleted ? 'Да' : 'Нет'; ?></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <?= Html::img($model->service->getImage()); ?>
 </div>

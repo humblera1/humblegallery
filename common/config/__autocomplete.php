5438ef0dd355ea27131d74e9ba987b7391abd7d2
maxim koshkin <humblerat@mail.ru>
2024-02-03 18:13:21 +0700

    _autocomplete

--- common/config/__autocomplete.php
+++ common/config/__autocomplete.php
@@ -1,33 +1,27 @@
 <?php
 
+use common\modules\user\models\data\User;
+use yii\rbac\DbManager;
+
+
 /**
  * This class only exists here for IDE (PHPStorm/Netbeans/...) autocompletion.
  * This file is never included anywhere.
  * Adjust this file to match classes configured in your application config, to enable IDE autocompletion for custom components.
- * Example: A property phpdoc can be added in `__Application` class as `@property \vendor\package\Rollbar|__Rollbar $rollbar` and adding a class in this file
- * ```php
- * // @property of \vendor\package\Rollbar goes here
- * class __Rollbar {
- * }
- * ```
  */
-class Yii {
-    /**
-     * @var \yii\web\Application|\yii\console\Application|__Application
-     */
-    public static $app;
+class Yii
+{
+    public static __Application $app;
 }
 
 /**
- * @property yii\rbac\DbManager $authManager 
- * @property \yii\web\User|__WebUser $user
- * 
+ * @property DbManager $authManager
+ * @property __WebUser $user
  */
-class __Application {
-}
+class __Application {}
 
 /**
- * @property app\models\User $identity
+ * @property User $identity
  */
 class __WebUser {
 }

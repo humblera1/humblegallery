c3acaf15426ad2bc9963358a26babe58e821d232
Maxim Koshkin <humblerat@mail.ru>
2024-12-24 23:55:49 +0700

    added style loaders, updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,15 +23,39 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    process.env.NODE_ENV !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+
+                    // 'postcss-loader', // Поддержка старых браузеров
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
         ],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
     },
     resolve: {
         alias: {
+            '@styles': path.resolve(__dirname, 'frontend/web/css'),
             '@artists': path.resolve(__dirname, 'web/js/artists/'),
             '@paintings': path.resolve(__dirname, 'web/js/paintings/'),
             '@modules': path.resolve(__dirname, 'web/js/modules/'),
         },
     },
+    // npm install --save-dev mini-css-extract-plugin
+    // plugins: [
+    //     new MiniCssExtractPlugin({
+    //         filename: '[name].[contenthash].css',
+    //     }),
+    // ],
     // devServer: {
     //     contentBase: path.resolve(__dirname, 'web'),
     //     compress: true,

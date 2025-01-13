f2f90627b32c47c47127a4da393c0c103bcfab2d
Maxim Koshkin <humblerat@mail.ru>
2025-01-14 01:36:39 +0700

    updated SelfHealingUrlFilter: added ability to customize the query

--- common/components/filters/SelfHealingUrlFilter.php
+++ common/components/filters/SelfHealingUrlFilter.php
@@ -2,13 +2,13 @@
 
 namespace common\components\filters;
 
+use Closure;
 use common\components\interfaces\SelfHealingUrlHandlerInterface;
 use common\components\urls\SelfHealingUrlHandler;
 use Yii;
 use yii\base\ActionFilter;
 use yii\base\InvalidConfigException;
 use yii\base\Model;
-use yii\db\ActiveRecord;
 use yii\di\NotInstantiableException;
 use yii\web\NotFoundHttpException;
 
@@ -20,7 +20,11 @@ class SelfHealingUrlFilter extends ActionFilter
 
     public Model|null $model = null;
 
+    public array|Closure $queryConstraints = [];
+
     private SelfHealingUrlHandlerInterface $handler;
+
+    private ?string $param = null;
 
     /**
      * @param $action
@@ -28,22 +32,18 @@ class SelfHealingUrlFilter extends ActionFilter
      * @throws InvalidConfigException
      * @throws NotFoundHttpException
      */
-    public function beforeAction($action)
+    public function beforeAction($action): bool
     {
         if ($this->modelClass === null) {
             throw new InvalidConfigException('You must specify the model class.');
         }
 
+        $this->setParam();
         $this->setHandler();
 
-        $param = Yii::$app->request->get($this->urlParamName);
-        $model = $this->modelClass::findOne($this->handler->getUniquePartFromUrl($param));
+        $model = $this->getModelInstance();
 
-        if ($model === null) {
-            throw new NotFoundHttpException();
-        }
-
-        if ($model->slug !== $this->handler->getSlugPartFromUrl($param)) {
+        if ($model->slug !== $this->handler->getSlugPartFromUrl($this->param)) {
             $this->owner->redirect($model->getSelfHealingUrl(), 301);
 
             return false;
@@ -52,6 +52,42 @@ class SelfHealingUrlFilter extends ActionFilter
         $this->model = $model;
 
         return parent::beforeAction($action);
+    }
+
+    /**
+     * @return Model
+     * @throws NotFoundHttpException
+     */
+    protected function getModelInstance(): Model
+    {
+        $pkName = $this->modelClass::primaryKey()[0];
+        $query = $this->modelClass::find()->where([$pkName => $this->handler->getUniquePartFromUrl($this->param)]);
+
+        if (is_callable($this->queryConstraints)) {
+            call_user_func($this->queryConstraints, $query);
+        } elseif (is_array($this->queryConstraints)) {
+            $query->andWhere($this->queryConstraints);
+        }
+
+        $model = $query->one();
+
+        if ($model === null) {
+            throw new NotFoundHttpException();
+        }
+
+        return $model;
+    }
+
+    /**
+     * @throws InvalidConfigException
+     */
+    protected function setParam(): void
+    {
+        $this->param = Yii::$app->request->get($this->urlParamName);
+
+        if (!$this->param) {
+            throw new InvalidConfigException('The URL parameter "%s" is required.', $this->urlParamName);
+        }
     }
 
     /**

c6eb39615cc16943f33eb61d61e6d9b1348bdde8
Maxim Koshkin <humblerat@mail.ru>
2025-01-15 01:04:43 +0700

    updated Collection model: added sluggable & selfHealingUrl behaviors, relation to retrieve first collection paintings

--- common/modules/collection/models/data/Collection.php
+++ common/modules/collection/models/data/Collection.php
@@ -2,26 +2,36 @@
 
 namespace common\modules\collection\models\data;
 
+use common\components\behaviors\SelfHealingUrlBehavior;
 use common\modules\collection\models\query\CollectionQuery;
 use common\modules\collection\models\service\CollectionService;
 use common\modules\painting\models\data\Painting;
+use common\modules\painting\models\data\PaintingCollection;
 use common\modules\user\models\data\User;
+use yii\base\InvalidConfigException;
+use yii\behaviors\SluggableBehavior;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
+use yii\db\Expression;
 
 /**
  *
  * @property int $id
  * @property string $title
+ * @property string $cover
+ * @property string $slug
  * @property int $user_id
  * @property bool $is_private
  * @property bool $is_archived
  * @property int $created_at
  *
  * @property User $user
+ * @property PaintingCollection[] $collectionPaintings
  * @property Painting[] $paintings
+ * @property Painting[] $firstPaintingsWithLimit Первые картины, сохраненные в коллекции.
  *
+ * @method SelfHealingUrlBehavior getSelfHealingUrl Generate a self-healing URL for the collection
  */
 
 class Collection extends ActiveRecord
@@ -43,12 +53,19 @@ class Collection extends ActiveRecord
         return '{{%collection}}';
     }
 
-    public function behaviors()
+    public function behaviors(): array
     {
         return [
             'timestampBehavior' => [
                 'class' => TimestampBehavior::class,
                 'createdAtAttribute' => false,
+            ],
+            'sluggable' => [
+                'class' => SluggableBehavior::class,
+                'attribute' => 'title',
+            ],
+            'selfHealingUrl' => [
+                'class' => SelfHealingUrlBehavior::class,
             ],
         ];
     }
@@ -80,10 +97,46 @@ class Collection extends ActiveRecord
         return $this->hasOne(User::class, ['id' => 'user_id']);
     }
 
+    public function getCollectionPaintings(): ActiveQuery
+    {
+        return $this->hasMany(PaintingCollection::class, ['collection_id' => 'id']);
+    }
+
+    /**
+     * @throws InvalidConfigException
+     */
     public function getPaintings(): ActiveQuery
     {
         return $this->hasMany(Painting::class, ['id' => 'painting_id'])
             ->viaTable('{{%painting_collection}}', ['collection_id' => 'id']);
+    }
+
+    /**
+     * Возвращает первые три картины для каждой коллекции на странице.
+     *
+     * @param int $limit
+     * @return ActiveQuery
+     * @throws InvalidConfigException
+     */
+    public function getFirstPaintingsWithLimit(int $limit = 3): ActiveQuery
+    {
+        $subquery = PaintingCollection::find()
+            ->select([
+                'collection_id',
+                'painting_id',
+                'created_at',
+                'row_number' => new Expression('ROW_NUMBER() OVER (PARTITION BY collection_id ORDER BY created_at)'),
+            ]);
+
+        return $this->hasMany(Painting::class, ['id' => 'painting_id'])
+            ->viaTable('painting_collection', ['collection_id' => 'id'], function($query) use ($subquery, $limit) {
+                $query->alias('pc')
+                    ->innerJoin(
+                        ['lp' => $subquery],
+                        'lp.painting_id = pc.painting_id AND lp.collection_id = pc.collection_id AND lp.row_number <= :limit',
+                        ['limit' => $limit]
+                    );
+            });
     }
 
     /** {@inheritdoc} */
